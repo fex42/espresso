9f17a4d89cf3998e7cce882f944046a639329bc5
Felix Na <felix.na70@gmail.com>
2024-02-25 21:56:52 +0100

    1st try

--- espresso.py
+++ espresso.py
@@ -57,7 +57,33 @@ with BuildPart() as cover:
             l5 = Line(l4@1, (0, height+wt))
             l6 = Line(l5@1, l1@0)
         make_face()
+        with Locations((0, height-4.0)):
+            Circle(4.2, mode=Mode.SUBTRACT)
     revolve(axis=Axis.Z)
 
+with BuildPart() as inner:
+    with BuildSketch(Plane.XZ) as cover_sk:
+        with BuildLine():
+            h = 7.5
+            h2 = height - wt
+            cl = Line((0, h), (r1, h), mode=Mode.PRIVATE)
+            l1 = Line((0,1.5), (r3-tol, 1.5))
+            l2 = Line(l1@1, ((l1@1).X, 4.5 + tol/2))
+            l3 = IntersectingLine(l2@1, Vector(0.5, 0.71), cl)
+            l4 = Line(l3@1, (2.5 ,h+3))
+            l5 = Line(l4@1, ((l4@1).X, h2))
+            l6 = Line(l5@1, (0,h2))
+            le = Line(l6@1, l1@0)
+        make_face()
+    revolve(axis=Axis.Z)
+    with Locations((0,0,height-4.2)):
+        Sphere(4.2)
+    fillet(inner.edges().filter_by_position(Axis.Z, 
+                                           minimum=10.0, 
+                                           maximum=height - 6), 
+                                           18.0)
 
-show(body, cover)
+
+
+
+show(body, cover, inner)
