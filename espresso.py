7978bb983d0d86ff3e1900aadef4847674b35116
Felix Na <felix.na70@gmail.com>
2024-02-25 19:37:32 +0100

    1st sketch of cup

--- espresso.py
+++ espresso.py
@@ -16,21 +16,26 @@ y1 = -inner_depth
 x2 = filter_dia / 2
 y2 = 0.0
 
-with BuildSketch() as sketch:
-    with BuildLine() as line:
-        l0 = Line((x1, 3), (x1, y1))
-        l1 = Line(l0@1, (x2, y1))
-        l2 = Line(l1 @ 1, (x2, y2))
-        l3 = ThreePointArc((l2 @ 1, l2 @ 1 + (filter_border_r, filter_border_r)), l2 @ 1 + (filter_border_r * 2, 0))
-        l4 = Line(l3 @ 1, l3 @ 1 + (1.5, 0))
-        l5 = Line(l4 @ 1, (x3, 2.0))
-        l5 = Line(l5 @ 1, (x3, 3.0))
-        l6 = ThreePointArc(l5 @ 1, l5 @ 1 + (-3, 8.5), l5 @ 1 + (0, 17))
-        l7 = Line(l6 @ 1, (x3, 70))
-        l8 = Line(l7 @ 1, l7 @ 1 + (-wt, 0))
-        l9 = Line(l8 @ 1, (x3 - wt, 30))
-        l10 = JernArc(l9@1, l9%1, radius=30,arc_size=-30)
-        l11 = Line(l10@1, l0@0)
+with BuildPart() as part:
+    with BuildSketch(Plane.XZ) as sketch:
+        with BuildLine() as line:
+            l0 = Line((x1, 3), (x1, y1))
+            l1 = Line(l0@1, (x2, y1))
+            l2 = Line(l1 @ 1, (x2, y2))
+            l3 = ThreePointArc((l2 @ 1, l2 @ 1 + (filter_border_r, filter_border_r)), l2 @ 1 + (filter_border_r * 2, 0))
+            l4 = Line(l3 @ 1, l3 @ 1 + (1.5, 0))
+            l5 = Line(l4 @ 1, (x3, 2.0))
+            l5 = Line(l5 @ 1, (x3, 3.0))
+            l6 = ThreePointArc(l5 @ 1, l5 @ 1 + (-3, 8.5), l5 @ 1 + (0, 17))
+            l7 = Line(l6 @ 1, (x3, 70))
+            l8 = Line(l7 @ 1, l7 @ 1 + (-wt, 0))
+            l9 = Line(l8 @ 1, (x3 - wt, 30))
+            l10 = JernArc(l9@1, l9%1, radius=30,arc_size=-30)
+            l11 = Line(l10@1, l0@0)
+        make_face()
+    revolve(axis=Axis.Z)
 
+IntersectingLine
 
-show(line)
+show(sketch)
+#show(part)
