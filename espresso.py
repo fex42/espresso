95774b32d340e3aad0fce5e1443800fd13150260
Felix Na <felix.na70@gmail.com>
2024-02-25 20:54:27 +0100

    use construction line

--- espresso.py
+++ espresso.py
@@ -2,40 +2,69 @@ from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, ge
 set_port(3939)
 from build123d import *
 
+wt = 2.0 # wall thickness
+tol = 0.4 # tolerance distance between parts
+
 inner_hole_dia = 48.0
 inner_depth = 1.5
-filter_dia = 58.0
 filter_border_r = 3.0
-wt = 2.0 # wall thickness
+
+
+
+
+
+
+
+
+inner_hole_dia = 48.0
+filter_dia1 = 58.0
+filter_border_w = 6.0
 outer_dia = 74.2
-x3 = outer_dia / 2
+
+height = 60.0
 
 
+r1 = outer_dia / 2
+r2 = filter_dia1 / 2
+r3 = inner_hole_dia / 2
+
+
+
+
+x3 = outer_dia / 2 # depr
 x1 = inner_hole_dia / 2
 y1 = -inner_depth
-x2 = filter_dia / 2
+x2 = filter_dia1 / 2
 y2 = 0.0
 
-with BuildPart() as part:
-    with BuildSketch(Plane.XZ) as sketch:
-        with BuildLine() as line:
-            l0 = Line((x1, 3), (x1, y1))
-            l1 = Line(l0@1, (x2, y1))
-            l2 = Line(l1 @ 1, (x2, y2))
-            l3 = ThreePointArc((l2 @ 1, l2 @ 1 + (filter_border_r, filter_border_r)), l2 @ 1 + (filter_border_r * 2, 0))
-            l4 = Line(l3 @ 1, l3 @ 1 + (1.5, 0))
-            l5 = Line(l4 @ 1, (x3, 2.0))
-            l5 = Line(l5 @ 1, (x3, 3.0))
-            l6 = ThreePointArc(l5 @ 1, l5 @ 1 + (-3, 8.5), l5 @ 1 + (0, 17))
-            l7 = Line(l6 @ 1, (x3, 70))
-            l8 = Line(l7 @ 1, l7 @ 1 + (-wt, 0))
-            l9 = Line(l8 @ 1, (x3 - wt, 30))
-            l10 = JernArc(l9@1, l9%1, radius=30,arc_size=-30)
-            l11 = Line(l10@1, l0@0)
+
+with BuildPart() as body:
+    with BuildSketch(Plane.XZ) as sketch2:
+        with BuildLine() as line2:
+            c1 = Line((r1 - wt, 0), (r1 - wt, height), mode=Mode.PRIVATE) # construction line
+            l1 = Line((r3, 0), (r3, 4.5))
+            l2 = IntersectingLine(l1@1, Vector(0.5, 0.71), c1)
+            l3 = Line(l2@1, ((l2@1).X, height))
+            l4 = Line(l3@1, (r1, height))
+
+            l5 = Line(l1@0, (r2 , (l1@0).Y))
+            l6 = PolarLine(l5@1, 1.5, 90.0)
+            l7 = ThreePointArc((l6@1, 
+                                l6@1 + (filter_border_r, filter_border_r)),
+                                l6@1 + (filter_border_r * 2, 0))
+            l8 = Line(l7@1, l7@1 + (1.5, 0))
+            l9 = Line(l8@ 1, (r1, 2.0))
+            l10 = Line(l9@ 1, (r1, 3.0))
+            l11 = ThreePointArc(l10@1, 
+                               l10@1 + (-3, 9), 
+                               l10@1 + (0, 18))
+            l12 = Line(l11@1, l4@1)
         make_face()
     revolve(axis=Axis.Z)
+    fillet(body.edges().filter_by_position(Axis.Z, 
+                                           minimum=10.0, 
+                                           maximum=40.0), 
+                                           30.0)
 
-IntersectingLine
 
-show(sketch)
-#show(part)
+show(body)
